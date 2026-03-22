9c87de9010876aa819ae02bbad12e223a675d227
Ozlem Tanrikulu <ozlemtanrikulu0@gmail.com>
2026-03-22 15:27:59 +0200

    add argocd terraform

---
+++ terraform/argocd/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "repo_url" {
+  type    = string
+  default = "https://github.com/tanrikuluozlem/burn"
+}
+
+variable "app_namespace" {
+  type        = string
+  description = "Namespace where burn app will be deployed"
+}

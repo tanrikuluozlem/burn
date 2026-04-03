5035be7d75231c3cf18692e3f34b5335ed20f423
Ozlem Tanrikulu <ozlemtanrikulu0@gmail.com>
2026-04-03 15:27:07 +0300

    add External Secrets Operator with IRSA

---
+++ terraform/argocd/external-secrets.tf
@@ -0,0 +1,101 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "external_secrets_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${data.terraform_remote_state.infra.outputs.oidc_provider}:sub"
+      values   = ["system:serviceaccount:external-secrets:external-secrets"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${data.terraform_remote_state.infra.outputs.oidc_provider}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    principals {
+      identifiers = [data.terraform_remote_state.infra.outputs.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "external_secrets" {
+  name               = "burn-external-secrets"
+  assume_role_policy = data.aws_iam_policy_document.external_secrets_assume.json
+}
+
+data "aws_iam_policy_document" "external_secrets_permissions" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret"
+    ]
+    resources = [
+      "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.current.account_id}:secret:burn/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "external_secrets" {
+  name   = "burn-external-secrets-policy"
+  policy = data.aws_iam_policy_document.external_secrets_permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "external_secrets" {
+  role       = aws_iam_role.external_secrets.name
+  policy_arn = aws_iam_policy.external_secrets.arn
+}
+
+resource "helm_release" "external_secrets" {
+  namespace        = "external-secrets"
+  name             = "external-secrets"
+  repository       = "https://charts.external-secrets.io"
+  chart            = "external-secrets"
+  version          = "0.10.0"
+  create_namespace = true
+  wait             = true
+
+  values = [
+    yamlencode({
+      serviceAccount = {
+        annotations = {
+          "eks.amazonaws.com/role-arn" = aws_iam_role.external_secrets.arn
+        }
+      }
+    })
+  ]
+}
+
+resource "kubectl_manifest" "cluster_secret_store" {
+  yaml_body = yamlencode({
+    apiVersion = "external-secrets.io/v1beta1"
+    kind       = "ClusterSecretStore"
+    metadata = {
+      name = "aws-secrets-manager"
+    }
+    spec = {
+      provider = {
+        aws = {
+          service = "SecretsManager"
+          region  = var.aws_region
+          auth = {
+            jwt = {
+              serviceAccountRef = {
+                name      = "external-secrets"
+                namespace = "external-secrets"
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+
+  depends_on = [helm_release.external_secrets]
+}

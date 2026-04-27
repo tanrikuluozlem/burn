509e4a7900280678797cf3611e7e8e5b5dc830b4
Ozlem Tanrikulu <ozlemtanrikulu0@gmail.com>
2026-04-27 13:32:12 +0300

    add oidc pricing role for   github actions

--- terraform/global/iam.tf
+++ terraform/global/iam.tf
@@ -22,3 +22,44 @@ resource "aws_iam_role_policy_attachment" "eks_cluster" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster.name
 }
+
+resource "aws_iam_role" "github_actions_pricing" {
+  name = "${local.project}-gh-pricing-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:tanrikuluozlem/burn:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_pricing" {
+  name = "pricing-read"
+  role = aws_iam_role.github_actions_pricing.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = [
+        "pricing:GetProducts",
+        "pricing:DescribeServices",
+        "pricing:GetAttributeValues"
+      ]
+      Resource = "*"
+    }]
+  })
+}
